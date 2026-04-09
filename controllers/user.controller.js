9de1f49379fe21229e5e4e8d4f5e997819b2f4b8
Victory <victoryajayi90@gmail.com>
2026-04-09 14:14:41 +0100

    first commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const Customer = require("../models/user.model");
 const ejs = require('ejs')
+const nodemailer = require('nodemailer');
 
 
 const getSignup = (req, res) => {
@@ -22,6 +23,52 @@ const postSignup = (req, res) => {
     newCustomer.save()
         .then((user) => {
             console.log("Customer saved:", user);
+
+            // Transporter means the informarion about the service you are using to send the email
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'adedayodaniel1711@gmail.com',
+                    // a special password generated from google settings not your original password
+                    // Step one: Enable 2-step verification
+                    // Step two: Generate app password
+                    pass: 'mawl exta bquu strt'
+                }
+            });
+
+            // This is the information about the email you are sending
+            let mailOptions = {
+                from: 'adedayodaniel1711@gmail.com',
+                to: [user.email, "victoryajayi90@gmail.com"],
+                subject: 'Welcome to our Application',
+                html: 
+                `
+                        <div style="background-color: #f4f4f4; padding: 0 0 10px; border-radius: 30px 30px 0 0  ;">
+                            <div style="padding-top: 20px; height: 100px; border-radius: 30px 30px 0 0 ; background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );">
+                                <h1 style="color:white; text-align: center;">Welcome to our Application</h1>
+                            </div>
+                            <div style="padding: 30px 0; text-align: center;">
+                                <p style="font-size: 18px;"><span style="font-weight: 600;">Congratulations!</span> Your sign-up was successful!</p>
+                                <p>Thank you for registering. We are excited to have you on board.</p>
+                                <div style="padding: 20px 0;">
+                                    <hr style="width: 50%;">
+                                    <p style="margin-bottom: 10px;">Best Regards</p>
+                                    <p style="color: #f89b29; margin-top: 0;">Dan Star</p>
+                                </div>
+                            </div>
+                        </div>
+                `
+                
+            };
+            // This is what will actually send the email
+            transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+            });
+
             res.redirect("/user/signin");
         })
         .catch((err) => {
@@ -39,10 +86,10 @@ const postSignin = (req, res) => {
                 console.log("Invalid email");
                 return res.status(400).json({message: "Invalid email or password"})
             } 
-            // if (foundCustomers.password !== password) {
-            //     console.log("Invalid Password");
-            //     return res.status(400).json({ message: "Invalid email or password"});
-            // }
+            if (foundCustomers.password !== password) {
+                console.log("Invalid Password");
+                return res.status(400).json({ message: "Invalid email or password"});
+            }
 
 
             // Success
