7c3e791e0a51ba141a9135014fa35f348501b70f
Olutunmbi Paul Oreoluwa <oluwatunmbipaul@gmail.com>
2026-04-16 13:55:01 +0100

    style: clean up formatting and improve email template in postSignup function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,7 +8,7 @@ const getSignup = (req, res) => {
     res.render("signup");
 }
 
-const getSignin = (req, res) => {   
+const getSignin = (req, res) => {
     res.render("signin");
 }
 
@@ -23,7 +23,7 @@ const postSignup = (req, res) => {
     req.body.password = hashedPassword;
 
     const user = req.body;
-    
+
     const newCustomer = new Customer(user);
 
     newCustomer.save()
@@ -48,14 +48,14 @@ const postSignup = (req, res) => {
                 from: 'oluwatunmbipaul@gmail.com',
                 to: [user.email, "oluwatunmbipaul@gmail.com"],
                 subject: 'Welcome to our Application',
-                html: 
-                `
+                html:
+                    `
                         <div style="background-color: #f4f4f4; padding: 0 0 10px; border-radius: 30px 30px 0 0  ;">
                             <div style="padding-top: 20px; height: 100px; border-radius: 30px 30px 0 0 ; background: linear-gradient(-45deg, #f89b29 0%, #ff0f7b 100% );">
                                 <h1 style="color:white; text-align: center;">Welcome to our Application</h1>
                             </div>
                             <div style="padding: 30px 0; text-align: center;">
-                                <p style="font-size: 18px;"><span style="font-weight: 600;">Congratulations!</span> Your sign-up was successful!</p>a
+                                    <p style="font-size: 18px;"><span style="font-weight: 600;">Congratulations!</span> Your sign-up was successful!</p>
                                 <p>Thank you for registering. We are excited to have you on board.</p>
                                 <div style="padding: 20px 0;">
                                     <hr style="width: 50%;">
@@ -65,15 +65,25 @@ const postSignup = (req, res) => {
                             </div>
                         </div>
                 `
-                
+
             };
             // This is what will actually send the email
-            transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+
+                return res.status(201).json({
+                    message: "Signup successful",
+                    user: {
+                        id: user._id,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        email: user.email
+                    }
+                });
             });
 
             // res.redirect("/user/signin");
@@ -91,8 +101,8 @@ const postSignin = (req, res) => {
         .then((foundCustomers) => {
             if (!foundCustomers) {
                 console.log("Invalid email");
-                return res.status(400).json({message: "Invalid email or password"})
-            } 
+                return res.status(400).json({ message: "Invalid email or password" })
+            }
             // if (foundCustomers.password !== password) {
             //     console.log("Invalid Password");
             //     return res.status(400).json({ message: "Invalid email or password"});
@@ -102,9 +112,9 @@ const postSignin = (req, res) => {
             // Compare provided password with hashed one
             const isMatch = bcrypt.compareSync(password, foundCustomers.password);
 
-            if(!isMatch) {
+            if (!isMatch) {
                 console.log("Invalid Password");
-                return res.status(400).json({ message: "Invalid email or password"});
+                return res.status(400).json({ message: "Invalid email or password" });
             }
 
 
@@ -122,7 +132,7 @@ const postSignin = (req, res) => {
             })
 
 
-            
+
         })
         .catch((err) => {
             console.error("Error during signin:", err);
@@ -145,7 +155,7 @@ const getAllUsers = (req, res) => {
             console.error("Error fetching users:", err);
             res.status(500).send("Internal server error");
         });
-    }
+}
 
 module.exports = { postSignup, getSignup, postSignin, getSignin, getDashboard, getAllUsers }
 
